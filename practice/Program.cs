a6f3c177c1eb253376202480efaf844b13e1aff5
Forrest-Flowers <forrest.w.flowers@gmail.com>
2019-01-14 19:00:15 -0600

    Attempt one at 2 decimal practice

---
+++ practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+
+            string userInput = Console.ReadLine();
+
+           string convertedInput = String.Format("{0:0.00}", userInput);
+
+            double finalResult = Convert.ToDouble(convertedInput);
+
+            Console.WriteLine(finalResult);
+
+            Console.Read();
+
+        }
+    }
+}
