f23c507517726244cac7fdf240bda063a1b2abbf
Forrest-Flowers <forrest.w.flowers@gmail.com>
2019-01-23 19:03:16 -0600

    Work from class

---
+++ Lesson03/StartOfLesson/ToDo/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Middleware
+{
+    public class LoggerMiddleware : IMiddleware
+    {
+        private readonly ILogger<LoggerMiddleware> _logger;
+        public LoggerMiddleware(ILogger<LoggerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            _logger.LogError("Middleware!!!");
+
+            await next(context);
+        }
+    }
+}
--- Lesson03/StartOfLesson/ToDo/Startup.cs
+++ Lesson03/StartOfLesson/ToDo/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ToDoApp.Middleware;
 
 namespace ToDoApp
 {
@@ -26,6 +28,7 @@ namespace ToDoApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddTransient < LoggerMiddleware>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -45,6 +48,7 @@ namespace ToDoApp
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            app.UseMiddleware<LoggerMiddleware>();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
